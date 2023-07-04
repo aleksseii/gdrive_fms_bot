dd999e32302636774d4389fb60c7bcda1dce57f4
Алексей Шкель <aleksseii>
2023-07-04 06:23:12 +0300

    Connected to telegram bot via its token

--- src/main.R
+++ src/main.R
@@ -1,8 +1,17 @@
 library(telegram.bot)
 library(googledrive)
 library(configr)
+library(dplyr)
 
 config <- read.config("creds/config.yml")
 bot_token <- config$telegram$bot$token
-bot_token
 
+bot <- Bot(token = bot_token)
+updates <- bot$getUpdates()
+
+chat_id <- updates[[1]]$from_chat_id()
+username <- updates[[1]]$message$chat$username
+
+bot$send_message(chat_id,
+                 text = paste0("Hello *", username, "*! I'm a _bot_!"),
+                 parse_mode = "Markdown")
